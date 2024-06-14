4607567710b8f9740121a308ab754fbcaae93b43
Terran <wuchaoboy@gmail.com>
2024-06-14 10:34:55 +0800

    Update header.tsx

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -30,7 +30,7 @@ const Header: FC<IHeaderProps> = ({
         )
         : <div></div>}
       <div className='flex items-center space-x-2'>
-        <AppIcon size="small" />
+{/*         <AppIcon size="small" /> */}
         <div className=" text-sm text-gray-800 font-bold">{title}</div>
       </div>
       {isMobile
